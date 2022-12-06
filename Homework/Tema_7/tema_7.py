1a4b49937a4566cf3c4ced98433f26624b21fe1d
Alexandru <alexandruioanbanciu@gmail.com>
2022-12-06 23:51:20 +0200

    homework

---
+++ Homework/Tema_7/tema_7.py
@@ -0,0 +1,7 @@
+'''ABSTRACTION
+Clasa abstractă FormaGeometrica
+Conține un field PI=3.14
+Conține o metodă abstractă aria (opțional)
+Conține o metodă a clasei descrie() - aceasta printează pe ecran ‘Cel mai
+probabil am colturi’'''
+
