3f8ce5eb2729076c4c7395c8c87690dbc49970cf
Alexandru <alexandruioanbanciu@gmail.com>
2022-12-07 16:12:46 +0200

    homework

---
+++ Homework/Tema_7/README.md
@@ -0,0 +1,54 @@
+Tema 7 - OOP _ Cei 4 Piloni
+
+Exerciții Recomandate - grad de dificultate: Ușor
+
+1. Revizualizează întâlnirea 7 și ia notițe în caz că ți-a scăpat ceva.
+
+Te rog să scrii pe canalul de comunicare scrisă unde întâmpini dificultăți și te
+ajut.
+Dacă stai blocat > 30 min, cere indiciu.
+
+1.Git setup
+Exerciții obligatorii - grad de dificultate: Usor spre Mediu
+
+1. Creează-ți cont de github și încarcă într-un repo nou tot ce am facut până
+acum la ore.
+Curs git/github https://bit.ly/3yfFvqL
+VIDEOS 1, 2 si 3
+
+2. Rezolvă exercițiul după ce ai urcat proiectul (tot ce am facut până acum
+împreună).
+
+ABSTRACTION
+Clasa abstractă FormaGeometrica
+Conține un field PI=3.14
+Conține o metodă abstractă aria (opțional)
+Conține o metodă a clasei descrie() - aceasta printează pe ecran ‘Cel mai
+probabil am colturi’
+
+INHERITANCE
+Clasa Pătrat - moștenește FormaGeometrica
+constructor pentru latură
+
+ENCAPSULATION
+latura este proprietate privată
+Implementează getter, setter, deleter pentru latură
+Implementează metoda cerută de interfață (opțional, doar dacă ai ales să
+implementezi metoda abstractă aria)
+Clasa Cerc - moștenește FormaGeometrica
+constructor pentru rază
+raza este proprietate privată
+Implementează getter, setter, deleter pentru rază
+Implementează metoda cerută de interfață - în calcul folosește field PI
+mostenit din clasa părinte (opțional, doar dacă ai ales să implementezi metoda
+abstractă aria)
+
+POLYMORPHISM
+Definește o nouă metodă descrie - printează ‘Eu nu am colturi’
+Creează un obiect de tip Patrat și joacă-te cu metodele lui
+Creează un obiect de tip Cerc și joacă-te cu metodele lui
+
+3. Actualizează proiectul pe github facand un push la noul cod
+În Foldereul de teme ajunge să pui doar linkul cu proiectul tău public
+Curs git/github
+https://bit.ly/3yfFvqL - VIDEO 4
--- Homework/Tema_7/tema_7.py
+++ Homework/Tema_7/tema_7.py
@@ -5,3 +5,97 @@ Conține o metodă abstractă aria (opțional)
 Conține o metodă a clasei descrie() - aceasta printează pe ecran ‘Cel mai
 probabil am colturi’'''
 
+from abc import ABC, abstractmethod
+
+class Forma_Geometrica(ABC):
+
+    PI = 3.14
+
+    @abstractmethod
+    def aria(self):
+        pass
+
+    @classmethod
+    def descrie(cls):
+        print('Cel mai probabil am colturi')
+
+#INHERITANCE
+# - Clasa Patrat - mosteneste Forma_Geometrica
+# - constructor pentru latura '''
+
+class Patrat(Forma_Geometrica):
+    def __init__(self, latura):
+        self.__latura = latura
+
+# ENCAPSULATION
+# - latura este propietate privata
+# - implementeaza getter,setter, deleter pentru latura
+# - implementeaza metoda ceruta de interfata (optional, doar daca ai ales sa implementezi metoda abstracta aria)'''
+
+    def aria(self):
+        return self.__latura * self.__latura
+
+    @property
+    def latura(self):
+        return self.__laturalatura
+
+    @latura.getter
+    def culoare(self):
+        print(f'Getter: latura este {self.__latura}')
+        return self.__latura
+
+    @latura.setter
+    def color(self, latura):
+        print(f'Setter: Noua latura este {latura}')
+        self.__latura = latura
+
+    @latura.deleter
+    def color(self):
+        print(f'Deleter: Am sters latura')
+        self.__latura = None
+
+
+'''Clasa Cerc - moștenește FormaGeometrica
+constructor pentru rază
+raza este proprietate privată
+Implementează getter, setter, deleter pentru rază
+Implementează metoda cerută de interfață - în calcul folosește field PI
+mostenit din clasa părinte (opțional, doar dacă ai ales să implementezi metoda
+abstractă aria)'''
+
+class Cerc(Forma_Geometrica):
+
+    def __init__(self, raza):
+        self.__raza = raza
+
+    @property
+    def raza(self):
+        return self.__raza
+
+    @raza.getter
+    def raza(self):
+        print(f'Raza cercului este: {self.__raza}')
+        return self.__raza
+
+    @raza.setter
+    def raza(self, raza):
+        print(f'Noua valoarea a razei este: {raza}')
+        self.__raza = raza
+
+    @raza.deleter
+    def raza(self):
+        print(f'Am sters valoarea razei')
+        self.__raza = 0
+
+    def aria(self):
+        aria_cercului = self.PI * self.__raza * self.__raza
+        return aria_cercului
+
+    '''POLYMORPHISM
+Definește o nouă metodă descrie - printează ‘Eu nu am colturi’
+Creează un obiect de tip Patrat și joacă-te cu metodele lui
+Creează un obiect de tip Cerc și joacă-te cu metodele lui'''
+
+    def descrie(self):
+        print(f'Eu nu am colturi')
+
